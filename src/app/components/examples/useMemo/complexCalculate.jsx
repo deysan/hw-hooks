2f3963deb328e6435899fb7bf3242d01ecbb7feb
deysan <ideysan@gmail.com>
2022-01-17 23:43:13 +0200

    Trainee useMemo

--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
+function factorial(n) {
+    return n ? n * factorial(n - 1) : 1;
+}
+
+function runFactorial(n) {
+    console.log("Run Factorial");
+    return factorial(n);
+}
+
 const ComplexCalculateExample = () => {
+    const [value, setValue] = useState(100);
+    const [otherState, setOtherState] = useState(false);
+
+    const buttonColor = otherState ? "success" : "danger";
+
+    useEffect(() => {
+        console.log("Render Button color");
+    }, [buttonColor]);
+
+    const fact = useMemo(() => runFactorial(value), [value]);
+
     return (
         <>
             <CardWrapper>
                 <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <p>Value: {value}</p>
+                <p>Result fact: {fact}</p>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => setValue((prevState) => prevState + 10)}
+                >
+                    Increment
+                </button>
+                <button
+                    className="btn btn-primary mx-3"
+                    onClick={() => setValue((prevState) => prevState - 10)}
+                >
+                    Decrement
+                </button>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
+                <button
+                    className={"btn mt-2 btn-" + buttonColor}
+                    onClick={() => setOtherState((prevState) => !prevState)}
+                >
+                    Change color
+                </button>
             </CardWrapper>
         </>
     );
