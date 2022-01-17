e8618f449db7e4eda09c1c1ac0260c90d2ddd10f
deysan <ideysan@gmail.com>
2022-01-17 23:11:14 +0200

    Trainee useRef

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
 import SmallTitle from "../../common/typografy/smallTitle";
+
 const PrevStateExample = () => {
+    const prevState = useRef("");
+    const [currentState, setCurrentState] = useState("false");
+
+    const toggleCurrentState = () => {
+        setCurrentState((prevState) =>
+            prevState === "false" ? "true" : "false"
+        );
+    };
+
+    useEffect(() => {
+        prevState.current = currentState;
+    }, [currentState]);
+
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <Divider />
+            <p>prev State: {prevState.current}</p>
+            <p>current State: {currentState}</p>
+            <button className="btn btn-primary" onClick={toggleCurrentState}>
+                Toggle other state
+            </button>
         </CardWrapper>
     );
 };
--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
 import SmallTitle from "../../common/typografy/smallTitle";
+
 const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+
+    const handleClickFocus = () => {
+        inputRef.current.focus();
+    };
+
+    const handleClickWidth = () => {
+        inputRef.current.style.width = "100px";
+    };
+
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
+            <Divider />
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                    Email
+                </label>
+                <input
+                    ref={inputRef}
+                    type="email"
+                    className="form-control"
+                    id="email"
+                />
+            </div>
+            <button className="btn btn-primary" onClick={handleClickFocus}>
+                Focus input
+            </button>
+            <button
+                className="btn btn-secondary mx-3"
+                onClick={handleClickWidth}
+            >
+                Edit width input
+            </button>
         </CardWrapper>
     );
 };
